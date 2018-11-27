afabadd69a48767ca677665fa9c226e29642e6b2
mrogulski <m.rogulski@outlook.com>
2018-11-27 10:57:06 +0100

    bikes availability

--- src/main/java/com/mokobike/controller/AvailableBikesController.java
+++ src/main/java/com/mokobike/controller/AvailableBikesController.java
@@ -3,9 +3,8 @@ package com.mokobike.controller;
 import com.mokobike.repository.BikeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import java.text.SimpleDateFormat;
 import java.util.HashMap;
-import java.util.TimeZone;
+
 
 
 @RestController
@@ -18,19 +17,12 @@ public class AvailableBikesController extends Controller {
     @RequestMapping(value = "/bikes/availability")
     @ResponseBody
     public HashMap<String, Integer> checkAvailableBikes(
-            @RequestParam(name="dateFrom") Long dateFrom,
-            @RequestParam(name="dateTo") Long dateTo,
+            @RequestParam(name="dateFrom")  String dateFrom,
+            @RequestParam(name="dateTo")  String  dateTo,
             @RequestParam(name="type")String type
     ){
-        SimpleDateFormat fromat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        fromat.setTimeZone(TimeZone.getTimeZone("Europe/Sarajevo"));
-
-        HashMap<String, Integer> availableBikes = new HashMap<String, Integer>();
-
-        String from = fromat.format(dateFrom);
-        String to = fromat.format(dateTo);
-
-        availableBikes.put(type.toLowerCase() + "_bikes", bikeRepository.findAvailableBikes(from, to, type));
+        HashMap<String, Integer> availableBikes = new HashMap<>();
+        availableBikes.put(type.toLowerCase() + "_bikes", bikeRepository.findAvailableBikes(dateFrom, dateTo, type));
 
         return availableBikes;
 
